2e1329f843035ad42bb89267a7c0ed9c89b37655
Guilherme Paulo <guilhermeps002@gmail.com>
2022-07-21 19:31:49 -0300

    folders of balance sheet model and chart of accounts has being created

---
+++ src/model/charts_of_accounts.py
@@ -0,0 +1,7 @@
+#PLANO DE CONTAS PARA O BANCO DE DADOS
+print("----- PLANO DE CONTAS -----")
+assets_accounts = {}
+while True:
+	assets_accounts['Nome da conta']=input('Digite o nome da conta: ')
+	break
+print(assets_accounts)
